7b5b83b7a34950c57fa1266ee24df896cc1321be
exv1z1 <huseynhuseynov278@gmail.com>
2026-09-08 21:32:30 +0400

    Delete auth directory

--- auth/logout.js
+++
@@ -1,8 +0,0 @@
-// POST /api/auth/logout
-import { json, clearSession } from '../_db.js';
-
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return json(res, 405, { error: 'Только POST' });
-  clearSession(res);
-  return json(res, 200, { ok: true });
-}
